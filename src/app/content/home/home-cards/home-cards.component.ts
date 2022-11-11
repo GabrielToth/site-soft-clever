379ee6333021021416e261116fd139fa3e60ebb8
Gabriel Toth Gonçalves <60109701+GabrielToth@users.noreply.github.com>
2022-11-11 13:08:17 -0300

    add

--- src/app/content/home/home-cards/home-cards.component.ts
+++ src/app/content/home/home-cards/home-cards.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home-cards',
   templateUrl: './home-cards.component.html',
   styleUrls: ['./home-cards.component.scss']
 })
-export class HomeCardsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class HomeCardsComponent {}
